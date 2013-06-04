80e6eb857e6b31d3905a6b949f03dbd62ae9a615
oetrog <omer.etrog@alcatel-lucent.com>
2013-06-04 16:24:58 +0300

    remove screens

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,14 +3,30 @@
 /* Controllers */
 
 
-function MyCtrl1($scope,$http) {
+function MyCtrl1($scope,$http,$location) {
     $scope.gameSelected = function(gameTypeName){
         $scope.gameType = gameTypeName;
     }
 
     $scope.submit = function(){
 
-        $http.get('http://172.23.34.52/requestgame.ashx?'+)
+        $http.get('http://172.23.34.52/requestgame.ashx?gameType='+ $scope.gameType+"&name="+$scope.name+"&email="+$scope.email).
+            success(function(data,status,headers,config){
+                if (data == 'pending'){
+                   alert(data);
+                }
+                if (data !='' && data !="pending"){
+                    $scope.partner = data;
+                    $location.path('/raffle');
+                }
+        }).
+        error(function (data, status, headers, config) {
+            alert(data);
+
+        });
+        $scope.gameType = null;
+        $scope.name = null;
+        $scope.email = null;
     }
 
 }
