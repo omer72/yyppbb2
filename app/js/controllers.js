c7960783f4766f0ff119a016e8ec047db9e3753c
oetrog <omer.etrog@alcatel-lucent.com>
2013-06-04 15:29:52 +0300

    remove screens

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,12 +3,14 @@
 /* Controllers */
 
 
-function MyCtrl1($scope) {
-
-
-
+function MyCtrl1($scope,$http) {
     $scope.gameSelected = function(gameTypeName){
         $scope.gameType = gameTypeName;
+    }
+
+    $scope.submit = function(){
+
+        $http.get('http://172.23.34.52/requestgame.ashx?'+)
     }
 
 }
